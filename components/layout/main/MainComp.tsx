36052c521e4ed8990ff40b1b2a6b58b57bb6f137
moonctp <moonctp24@gmail.com>
2024-01-21 20:53:48 +0900

    메인페이지 퍼블완료

--- components/layout/main/MainComp.tsx
+++ components/layout/main/MainComp.tsx
@@ -1,9 +1,27 @@
 import type { NextPage } from "next";
+import style from "./MainComp.module.css";
 
 const MainComp: NextPage = () => {
   return (
     <>
-      <div>test page</div>
+      <div className="mainContentsLayout">
+        <div className="mainPageContentsArea">
+          <div className={style.mainPageButtonList}>
+            <div className={style.buttonLayout}>
+              <button className="btn_white">대시보드</button>
+            </div>
+            <div className={style.buttonLayout}>
+              <button className="btn_white">회원관리</button>
+            </div>
+            <div className={style.buttonLayout}>
+              <button className="btn_white">의약품관리</button>
+            </div>
+            <div className={style.buttonLayout}>
+              <button className="btn_white">관리자설정</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
